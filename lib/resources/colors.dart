7a526d66a916f81bc65186e454d83b356e6ea87a
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-01-01 09:17:55 +0700

    add init resources

---
+++ lib/resources/colors.dart
@@ -0,0 +1,55 @@
+import 'dart:ui';
+
+class Colours {
+  const Colours._();
+
+  /// #EDF8FF #FDC1E8, #FDFAE1, #FFFFFF
+  static const gradient = [
+    Color(0xFFEDF8FF),
+    Color(0xFFFDC1E8),
+    Color(0xFFFFFFFF),
+    Color(0xFFFDFAE1),
+  ];
+
+  /// #458CFF
+  static const primaryColour = Color(0xFF458CFF);
+
+  /// #757C8E
+  static const neutralTextColour = Color(0xFF757C8E);
+
+  /// #0305FE
+  static const physicsTileColour = Color(0xFFD3D5FE);
+
+  /// #FFEFDA
+  static const scienceTileColour = Color(0xFFFFEFDA);
+
+  /// #FFE4F1
+  static const chemistryTileColour = Color(0xFFFFE4F1);
+
+  /// #CFE5FC
+  static const biologyTileColour = Color(0xFFCFE5FC);
+
+  static const mathTileColour = Color(0xFFFFCECA);
+
+  /// #DAFFD6
+  static const languageTileColour = Color(0xFFDAFF06);
+
+  /// #05BEFB
+  static const literatureTileColour = Color(0xFFD5BEFB);
+
+  /// #FF5C5C
+  static const redColour = Color(0xFFFF5C5C);
+
+  /// #28CA6C
+  static const greenColour = Color(0xFF28CA6C);
+
+  /// #F4F5F6
+  static const chatFieldColour = Color(0xFFF4F5F6);
+
+  /// #EBE9EA
+  static const chatFieldColourDarker = Color(0xFFE8E9EA);
+  static const currentUserChatBubbleColour = Color(0xFF2196F3);
+  static const otherUserChatBubbleColour = Color(0xFFEEEEEE);
+  static const currentUserChatBubbleColourDarker = Color(0xFF197602);
+  static const otherUserChatBubbleColourDarker = Color(0xFFE0E0E0);
+}
